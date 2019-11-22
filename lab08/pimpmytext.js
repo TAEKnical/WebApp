fd3b0936d22db39f36e9b6724317b788cfd12f1e
TAEKnical <ilikebigmac@naver.com>
2019-11-22 22:30:06 +0900

    lab 08 finished

--- lab08/pimpmytext.js
+++ lab08/pimpmytext.js
@@ -1,4 +1,11 @@
-window.onload = pageload;
+window.onload = function(){
+	pageload();
+	$("bigger").onclick = fonttimer;
+	$("blingcheck").onchange = ifchecked;
+	$("snoopcheck").onchange = makeupper;
+	$("igpay").onclick = piglatin; 
+	$("malkovich").onclick = malkobich;
+};	
 var clickedBigger = 0;
 
 function pageload(){
@@ -7,44 +14,78 @@ function pageload(){
 	myTextArea.setAttribute('cols',30);
 }
 
-function fonttimer(){
-	//$("myTextArea").style.fontSize = "24pt";
-	var bigger = document.getElementById("bigger");
-	if(clickedBigger == 0){
-		bigger.addEventListener('click', function(event){
-			alert(1);
-			//var myTextArea = document.getElementById("myTextArea");
-			//myTextArea.style.fontSize="12pt";
-		});
+function defaultsize(){
+	var fontsize = document.getElementById("myTextArea").style.fontSize;
+	if(!fontsize){
+		$("myTextArea").style.fontSize = "14pt";		
 	}
 	else{
-		bigger.addEventListener('click', function(event){
-			alert(2);
-			//var myTextArea = document.getElementById("myTextArea");
-			//myTextArea.style.fontSize="14pt";
-		});
+		$("myTextArea").style.fontSize = parseInt($("myTextArea").style.fontSize)+2+"pt";
 	}
-	clickedBigger++;
 }
 
+function fonttimer(){
+	setInterval(function(){
+		defaultsize();
+	}, 500);
+}
+
+
 function ifchecked(){
-	var blingcheck = document.getElementById("blingcheck");
 	if(blingcheck.checked){
 		myTextArea.style.fontWeight = "bold";
 		myTextArea.style.color = "green";
 		myTextArea.style.textDecoration = "underline";
+		document.body.style.backgroundImage = 'url("./hundred.jpg")';
 	}
 	else{
 		myTextArea.style.fontWeight = "normal";
 		myTextArea.style.color = "green";
 		myTextArea.style.textDecoration = "underline";
+				document.body.style.backgroundImage = "none";
 	}
 }
 
 function makeupper(){
-	var myTextArea = document.getElementById("myTextArea")
+	var myTextArea = document.getElementById("myTextArea");
 	myTextArea.value = myTextArea.value.toUpperCase();
 	
 	var sArray = myTextArea.value.split(".");
 	myTextArea.value = sArray.join("-izzle.");
+}
+
+function piglatin(){
+	var myTextArea = document.getElementById("myTextArea");
+	myTextArea.value= myTextArea.value.toLowerCase();
+	var vowel = ['a','i','o','u','e'];
+	var s = "";
+	var i = 0;
+	var index = 0;
+
+	if(vowel.includes(myTextArea.value[0])){
+		s = myTextArea.value+"way";
+	}
+	else{
+		for(let char of myTextArea.value){
+			if(vowel.includes(char)){
+				index = myTextArea.value.indexOf(char);
+				break;
+			}
+		}
+		s = myTextArea.value.substring(index,myTextArea.value.length) + myTextArea.value.substring(0,index) + "ay";
+	}
+	myTextArea.value = s;
+}
+
+function malkobich(){
+	var myTextArea = document.getElementById("myTextArea");
+	var sArray = myTextArea.value.split(" ");
+
+	for(let word of sArray){
+		if(word.length > 5){
+			sArray[sArray.indexOf(word)] = "Malkovich";
+		}
+	}
+
+	myTextArea.value = sArray.join(" ");
 }
